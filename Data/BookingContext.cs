41445e4db45d411c2849952a7e01fdfe3a42da49
Shivani <shivanijadhav2801@gmail.com>
2022-11-20 17:06:36 +0530

    WebApi

---
+++ Data/BookingContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebApiClassone.Models;
+
+namespace WebApiClassone.Data
+{
+    public class BookingContext : DbContext
+    {
+        public BookingContext(DbContextOptions options) : base(options)
+        {
+        }
+        public DbSet<Booking> Bookings { get; set; }
+    }
+}
