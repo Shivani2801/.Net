38e678cd2e73be072def02749d828b3a9cf4388e
Shivani <shivanijadhav2801@gmail.com>
2022-11-20 19:45:23 +0530

    Add all my files

---
+++ ShoppingCart/Controllers/ShoppingController.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Net;
+using ShoppingCart.Models;
+
+namespace ShoppingCart.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ShoppingController : ControllerBase
+    {
+        IList<Shopping> product = new List<Shopping>()
+        {
+            new Shopping()
+        {
+            ItemId=101,
+            ItemName="Iphone",
+            price=80000
+        },
+            new Shopping()
+        {
+            ItemId=102,
+            ItemName="TV",
+            price=15000
+        },
+            new Shopping()
+        {
+            ItemId=103,
+            ItemName="Washing Machine",
+            price=25000
+        },
+            new Shopping()
+        {
+            ItemId=104,
+            ItemName="Laptop",
+            price=35000
+        },
+
+        };
+        [HttpGet]
+        [Route("/getall")]
+        public IList<Shopping> GetAllItems()
+        {
+            return product;
+        }
+        [HttpGet]
+        [Route("/getitembyid/{id}")]
+        public Shopping GetItem(int id)
+        {
+            return product.FirstOrDefault(e => e.ItemId == id);
+        }
+        [HttpDelete]
+        [Route("/deleteitembyid/{id}")]
+        public IList<Shopping> DeleteItem(int id)
+        {
+            product.Remove(product.FirstOrDefault(e => e.ItemId == id));
+            return product;
+        }
+    }
+}
---
+++ ShoppingCart/Models/Shopping.cs
@@ -0,0 +1,9 @@
+﻿namespace ShoppingCart.Models
+{
+    public class Shopping
+    {
+        public int ItemId { get; set; }
+        public string ItemName { get; set; }
+        public int price { get; set; }
+    }
+}
